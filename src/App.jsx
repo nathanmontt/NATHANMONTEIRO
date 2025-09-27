b0662d9d8ebfd56889502a11dbca97fb0c96199d
nathanmontt <contato.mont@gmail.com>
2025-09-27 09:08:20 -0300

    Code base

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import './assets/styles/global.css'
-import Header from './assets/components/layout/Header'
 
 function App() {
   return (
     <>
-      <Header />
+      <div id="container-flex">
+        <div id="container-top-left-content">
+          <div id="container-texts-info">
+            <p id="dev-name"></p>
+            <p id="dev-ocupation"></p>
+          </div>
+
+          <div id="container-text-about">
+            <p id="dev-about"></p>
+          </div>
+
+          <div id="container-xp"></div>
+
+          <div id="container-social-icons"></div>
+
+          <div id="container-who-made"></div>
+        </div>
+        <div id="container-bottom-right-content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, animi.
+        </div>
+      </div>
     </>
   )
 }
